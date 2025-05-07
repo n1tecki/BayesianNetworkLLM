3c082adef477d81759dd52722912414b85b3cebe
DEVSGINFO\NITECKIM <M.Nitecki@iaea.org>
2025-05-07 17:10:10 +0200

    adjusted code

--- src/dbn/main.py
+++ src/dbn/main.py
@@ -1,9 +1,10 @@
 from src.dbn.dbn_training import dbn_train, flatten_df
-from dbn.evaluation import predict_df_data, steps_to_threshold, compare_single_threshold, compare_var_thresholds, compare_two_models, plot_accuracy_bars
+from src.dbn.evaluation import predict_df_data, steps_to_threshold, compare_single_threshold, compare_var_thresholds, compare_two_models, plot_accuracy_bars
 from src.dbn.graph_visualisation import network_visualisation
 from src.dbn.utils import split_train_test
 import matplotlib.pyplot as plt
 import pandas as pd
+import json
 
 
 # ---------- LOAD DATA ----------------------------------------
@@ -60,29 +61,43 @@ predictions_dict = predict_df_data(df_test,
 
 # ---------- EVALUATE PREDICTIONS ---------------------------------
 # Load SOFA classifications
-sofa_df = pd.read_parquet("data/semisupervised_df_classified")
+sofa_df = pd.read_parquet("data/sofa_df_classified.parquet")
+with open('data/predictions.json') as f:
+    predictions_dict = json.load(f)
 sofa_df.index.name = 'hadm_id'
 
 # Compare DBN probabilities to SOFA scores
 dbn_thr = .7
+# Neutral decision boundry would be 0.5
 sofa_thr = 2
 runs = []
+correct_runs = []
 dbn_sepsis_preds = []
 sofa_sepsis_preds = []
+correct_dbn_sepsis_preds = []
+correct_sofa_sepsis_preds = []
 ground_truth_sepsis = []
 for hadm in test_ids:
     # Sepsis timeline
-    y_dbn = list(predictions_dict[hadm].values())
-    y_sofa = sofa_df.loc[hadm, 'sofa'].tolist()
+    y_dbn = list(predictions_dict[str(hadm)].values())
+    y_sofa = sofa_df.loc[hadm, 'sofa_total'].tolist()
     runs.append((y_dbn, y_sofa))
 
     # Sepsis classification
     dbn_sepsis_preds.append(1 if any(x >= dbn_thr for x in y_dbn) else 0)
-    sofa_sepsis_preds.append(1 if any(x >= sofa_thr for x in y_dbn) else 0)
+    sofa_sepsis_preds.append(1 if any(x >= sofa_thr for x in y_sofa) else 0)
     ground_truth_sepsis.append(sofa_df.loc[hadm, 'sepsis'].iloc[0])
 
+# Get only correct predictions
+for idx, truth in enumerate(ground_truth_sepsis):
+    if (dbn_sepsis_preds[idx] == truth and
+        sofa_sepsis_preds[idx] == truth):
+        correct_dbn_sepsis_preds.append(runs[idx][0])
+        correct_sofa_sepsis_preds.append(runs[idx][1])
+        correct_runs.append(runs[idx])
+
 # Single-threshold comparison
-earlier, later, equal, deltas = compare_single_threshold(runs, dbn_thr, sofa_thr)
+earlier, later, equal, deltas = compare_single_threshold(correct_runs, dbn_thr, sofa_thr)
 
 # Sweep 0.5-1.0 and plot proportion where y_dbn is earlier
 thr, earlier_p, later_p, equal_p = compare_var_thresholds(runs)
@@ -98,4 +113,4 @@ print(metrics[["accuracy", "acc_CI_low", "acc_CI_high",
 print("\nMcNemar's χ² = {:.4f}, p-value = {:.4f}"
       .format(mcnemar_res.statistic, mcnemar_res.pvalue))
 
-plot_accuracy_bars(metrics, title="DBN vs SOFA – Sepsis Detection")
+plot_accuracy_bars(metrics, title="DBN vs SOFA - Sepsis Detection")
